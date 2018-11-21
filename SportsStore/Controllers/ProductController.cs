da0a0404f4467bf4409f2e5894b07228698f9e90
Indy Van Canegem <indy.vancanegem@student.hogent.be>
2018-11-21 10:18:17 +0100

    Add ProductController and Product View

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -1,16 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models.Domain;
 
-namespace SportsStore.Controllers
-{
-    public class ProductController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
+namespace SportsStore.Controllers {
+    public class ProductController : Controller {
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository) {
+            _productRepository = productRepository;
+        }
+        public IActionResult Index() {
+            return View(_productRepository.GetAll());
         }
     }
 }
